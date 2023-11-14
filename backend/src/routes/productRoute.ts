6b5b6c7f436c790ca28e97b951c86d515484b8b6
Ahmed Walid <ahmedwalid98.aw@gmail.com>
2023-11-14 22:31:43 +0200

    completed the post and put endpoints for products

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -1,10 +1,18 @@
 import { Router } from "express";
-import { getAllProducts, getProductById, deleteProductById } from "../controllers/ProductController";
+import { 
+  getAllProducts,
+  getProductById,
+  deleteProductById,
+  createProduct,
+  updateProduct
+} from "../controllers/ProductController";
 
 const router = Router()
 
 router.get('/products', getAllProducts)
+router.post('/products', createProduct)
 router.get('/products/:id', getProductById)
 router.delete('/products/:id', deleteProductById)
+router.put('/products/:id', updateProduct)
 
 export default router
--- backend/src/utils/valiation.ts
+++ backend/src/utils/valiation.ts
@@ -3,3 +3,18 @@ import * as Joi from 'joi'
 export const categorySchema = Joi.object({
   category_name: Joi.string().required()
 })
+
+export const productSchema = Joi.object({
+  product_name: Joi.string().required(),
+  product_desc: Joi.string().required(),
+  product_price: Joi.number().required(),
+  product_amount: Joi.number().required(),
+  category_name: Joi.string().required()
+})
+
+export const updateSchema = Joi.object({
+  product_name: Joi.string().optional(),
+  product_desc: Joi.string().optional(),
+  product_price: Joi.number().optional(),
+  product_amount: Joi.number().optional(),
+})
