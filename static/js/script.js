cf41ad84b004fcdec50edc9d2d5a474b92556e2d
Abdelrahman <abdelrahmanmoussa111@gmail.com>
2023-11-14 17:31:39 +0200

    hey from Dashboard

---
+++ static/js/script.js
@@ -0,0 +1,52 @@
+/* Start nav buttons */
+const categories = document.getElementById("categories");
+const products = document.getElementById("products");
+const orders = document.getElementById("orders");
+const users = document.getElementById("user");
+/* End nav buttons */
+/* Start sections */
+const categories_section = document.querySelector(".category-section");
+const products_section = document.querySelector(".product-section");
+const orders_section = document.querySelector(".order-section");
+const users_section = document.querySelector(".user-section");
+const head_title = document.querySelector(".head-title");
+/* End sections */
+
+function showSection(section, button) {
+    // Hide all sections
+    categories_section.style.cssText = "display: none;";
+    products_section.style.cssText = "display: none;";
+    orders_section.style.cssText = "display: none;";
+    users_section.style.cssText = "display: none;";
+
+    // Display the selected section
+    if (section) {
+        section.style.cssText = "display: flex;";
+        head_title.textContent = section.dataset.title || "";
+    }
+
+    // Remove "active" class from all buttons
+    categories.classList.remove("active");
+    products.classList.remove("active");
+    orders.classList.remove("active");
+    users.classList.remove("active");
+
+    // Add "active" class to the clicked button
+    button.classList.add("active");
+}
+
+categories.addEventListener("click", function () {
+    showSection(categories_section, categories);
+});
+
+products.addEventListener("click", function () {
+    showSection(products_section, products);
+});
+
+orders.addEventListener("click", function () {
+    showSection(orders_section, orders);
+});
+
+users.addEventListener("click", function () {
+    showSection(users_section, users);
+});
